496b7e4242627dc18d554918e23e9c7d5ba8396b
sharda maurya <shardamaurya42570@gmail.com>
2023-08-16 22:12:13 +0530

    pattern problems

---
+++ 3number_pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1, j;
+    while(i<=4)
+    {
+        j=1;
+        while(j<=4)
+        {
+            cout<<j<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+
+    }
+}
