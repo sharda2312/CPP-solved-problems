d4884cd5070b0488c52cddc06cb12d70f9a87a98
sharda maurya <shardamaurya42570@gmail.com>
2023-08-16 22:11:04 +0530

    pattern problems

---
+++ 2number_pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1, j;
+    while(i<=3)
+    {
+        j=1;
+        while(j<=3)
+        {
+            cout<<i<<" ";
+            j++;
+
+        }
+        cout<<endl;
+        i++;
+        
+    }
+}
