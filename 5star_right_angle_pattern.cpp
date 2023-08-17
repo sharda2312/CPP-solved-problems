357eae9f38ab4766cece6a629e7675a5da07bdcc
sharda maurya <shardamaurya42570@gmail.com>
2023-08-17 22:19:29 +0530

    pattern problems

---
+++ 5star_right_angle_pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,j;
+    while(i<=4)
+    {
+        j=1;
+        while(j<=i)
+        {
+            cout<<"* ";
+            j++;
+
+        }
+        cout<<endl;
+        i++;
+
+    }
+}
